e87110201aaaf8f18c877cbfa49047bf4507392f
Chengxi Yang <yangchen@oregonstate.edu>
2020-02-16 18:13:57 -0800

    Test n-queens 7

--- nqueens/create_data.py
+++ nqueens/create_data.py
@@ -8,18 +8,18 @@ def plance_queen(n, pre):
     else:
         possible = []
         all_ans = []
-        for row in range(n):
+        for col in range(n):
             success_flag = True
-            for pre_col, pre_row in enumerate(pre):
-                if pre_row == row:
+            for pre_row, pre_col in enumerate(pre):
+                if pre_col == col:
                     success_flag = False
                     break
-                col = len(pre)
-                if abs(pre_row - row) == col - pre_col:
+                row = len(pre)
+                if abs(pre_col - col) == row - pre_row:
                     success_flag = False
                     break
             if success_flag:
-                possible.append(row)
+                possible.append(col)
         for one in possible:
             new_pre = pre.copy()
             new_pre.append(one)
@@ -33,14 +33,18 @@ def to_boad(ans):
         board[i, x] = 1
     return board
 
-def to_mask(ans, n):
+def get_queen_col_from_fixed_row(board, row):
+    return np.argmax(board[row])
+
+def to_mask(ans, fixed, n):
     board = np.zeros(shape=(n,n), dtype=np.int)
-    for row in ans:
-        board[row] = 1
+    for row in fixed:
+        col = get_queen_col_from_fixed_row(ans, row)
+        board[row, col] = 1
     return board
 
 def get_problem(example, pre_fixed_bits):
-    example_blur = [set() for _ in range(len(example))]
+    example_fixed = [set() for _ in range(len(example))]
     flexible_bits = {i for i in range(len(example[0]))}
     flexible_bits = flexible_bits.difference(set(pre_fixed_bits))
     for bit in flexible_bits:
@@ -52,19 +56,20 @@ def get_problem(example, pre_fixed_bits):
                 new_fixed_bits = pre_fixed_bits + [bit]
                 for i, one_satisfied in enumerate(satisfied):
                     ori_idx = example.index(one_satisfied)
-                    satisfied_blur = get_problem(satisfied, new_fixed_bits)
-                    example_blur[ori_idx] = example_blur[ori_idx].union(satisfied_blur[i])
+                    satisfied_fixed = get_problem(satisfied, new_fixed_bits)
+                    example_fixed[ori_idx] = example_fixed[ori_idx].union(satisfied_fixed[i])
             else:
                 idx = example.index(satisfied[0])
-                example_blur[idx].add(tuple(pre_fixed_bits + [bit]))
-    return example_blur
+                example_fixed[idx].add(tuple(pre_fixed_bits + [bit]))
+    return example_fixed
 
 
-for n in range(4, 9):
+
+for n in range(8, 9):
     ans = plance_queen(n, [])
     if len(ans) > 0:
-        blur = get_problem(ans, [])
-        n_example = sum([len(item) for item in blur])
+        fixed = get_problem(ans, [])
+        n_example = sum([len(item) for item in fixed])
         print(f"Board Size: {n}, Num Solution: {len(ans)}, Num Blur: {n_example}")
 
         dir = str(n)
@@ -74,12 +79,17 @@ for n in range(4, 9):
         X = torch.zeros(size=(n_example, n, n), dtype=torch.float32)
         is_input = torch.zeros(size=(n_example, n, n), dtype=torch.int32)
         cnt = 0
-        for one_ans, one_ans_blur in zip(ans, blur):
-            print(one_ans, len(one_ans_blur))
-            X[cnt:cnt+len(one_ans)] = torch.from_numpy(to_boad(one_ans))
-            for one_blur in one_ans_blur:
-                is_input[cnt] = torch.from_numpy(to_mask(one_blur, n))
+        for one_ans, one_ans_fixed in zip(ans, fixed):
+            print(one_ans, len(one_ans_fixed))
+            board = to_boad(one_ans)
+            X[cnt : cnt+len(one_ans_fixed)] = torch.from_numpy(board)
+            # print(X[cnt])
+            for one_fixed in one_ans_fixed:
+                is_input[cnt] = torch.from_numpy(to_mask(board, one_fixed, n))
+                # print(is_input[cnt])
+                # print(one_fixed)
                 cnt += 1
         assert cnt == n_example
         torch.save(X, dir + "/features.pt")
         torch.save(is_input, dir + "/is_input.pt")
+
